acf1e7196050bd6ff4731358f8058035d4d9ac87
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-01-16 11:17:26 +0530

    Add files via upload

---
+++ checking and conversion.py
@@ -0,0 +1,19 @@
+# CHECKING AND CONVERSION
+
+# Type Checking
+# string
+print(type("super"))
+
+# integer
+print(type(123456))
+
+# float
+print(type(125.77))
+
+# boolean
+print(type(False))
+
+# Type Conversion
+# print("Number of letters in your name: " + len(input("Enter your name")))
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
